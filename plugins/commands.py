d6190cb611388dc25f94c3366ba5bad48f14d521
amalkrishna007 <81072934+amalkrishna007@users.noreply.github.com>
2022-02-06 17:17:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -195,4 +195,4 @@ async def bot_info(bot, message):
             InlineKeyboardButton('Source Code', url='https://github.com')
         ]
         ]
-    await message.reply(text="Language : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text="Language : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com'>Click here</a>\nUpdate Channel : <a href='https://t.me/GrandCinemas'>Update</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
