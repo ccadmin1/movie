13925a1a989d80200d16840ad876a8b1769b8547
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2022-05-31 19:55:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -8,6 +8,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
+BOT_PICS = (environ.get('PICS','')).split()
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
